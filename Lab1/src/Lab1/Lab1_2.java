f07d7d0e12c2f0fae2a7f23c2cb981b15b2fc01c
jirayuwongtom <lovelava600@gmail.com>
2025-11-20 17:58:16 +0700

    Submit Lab

---
+++ Lab1/src/Lab1/Lab1_2.java
@@ -0,0 +1,14 @@
+package Lab1;
+import java.util.Scanner;
+public class Lab1_2 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		double W = input.nextInt();
+		double H = input.nextInt();
+		double Area = W * H;
+		System.out.println(Area);
+		input.close();
+	}
+
+}
