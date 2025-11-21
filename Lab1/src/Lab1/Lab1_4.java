ef776a182183a89e5d3897d9ca8f7fed5b1a9479
jirayuwongtom <lovelava600@gmail.com>
2025-11-21 22:27:55 +0700

    Submit Lab

--- Lab1/src/Lab1/Lab1_4.java
+++ Lab1/src/Lab1/Lab1_4.java
@@ -4,18 +4,19 @@ public class Lab1_4 {
 	
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		System.out.print("Enter score : ");
 		int score = input.nextInt();
 		
-		if (score >= 80 && score <= 100) {
+		if (score >= 80 ) {
 			System.out.println("A");
 		}
-		else if (score >= 70 && score <= 79) {
+		else if (score >= 70) {
 			System.out.println("B");
 		}
-		else if (score >= 60 && score <= 69) {
+		else if (score >= 60 ) {
 			System.out.println("C");
 		}
-		else if (score >= 50 && score <= 59) {
+		else if (score >= 50 ) {
 			System.out.println("D");
 		}
 		else {
