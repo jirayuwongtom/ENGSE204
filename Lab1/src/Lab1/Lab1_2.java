d45d3b61f23c8b934ccc17588af222bf29928d44
jirayuwongtom <lovelava600@gmail.com>
2025-11-21 18:22:36 +0700

    Submit Lab

--- Lab1/src/Lab1/Lab1_2.java
+++ Lab1/src/Lab1/Lab1_2.java
@@ -5,10 +5,10 @@ public class Lab1_2 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter Width : ");
-		double W = input.nextDouble();
+		double w = input.nextDouble();
 		System.out.print("Enter Height : ");
-		double H = input.nextDouble();
-		System.out.println("Area = " + (W * H));
+		double h = input.nextDouble();
+		System.out.println("Area = " + (w * h));
 		input.close();
 	}//end main function
 }//end class
