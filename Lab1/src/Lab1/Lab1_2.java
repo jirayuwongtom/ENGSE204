a7f8f78a4a9013e905a85a485241019fb720c9b7
jirayuwongtom <lovelava600@gmail.com>
2025-11-21 22:53:00 +0700

    Submit Lab

--- Lab1/src/Lab1/Lab1_2.java
+++ Lab1/src/Lab1/Lab1_2.java
@@ -4,11 +4,14 @@ public class Lab1_2 {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		
 		System.out.print("Enter Width : ");
 		double w = input.nextDouble();
+		
 		System.out.print("Enter Height : ");
 		double h = input.nextDouble();
-		System.out.println("Area = " + (w * h));
+		
+		System.out.printf("Area = %.2f" , w * h);
 		input.close();
-	}//end main function
-}//end class
+	}
+}
