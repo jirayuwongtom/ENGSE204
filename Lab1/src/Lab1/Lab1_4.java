87da954679ed256bc994b2883b4558506d342e2c
jirayuwongtom <lovelava600@gmail.com>
2025-11-21 22:36:28 +0700

    Submit Lab

--- Lab1/src/Lab1/Lab1_4.java
+++ Lab1/src/Lab1/Lab1_4.java
@@ -3,25 +3,28 @@ import java.util.Scanner;
 public class Lab1_4 {
 	
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Enter score : ");
-		int score = input.nextInt();
-		
-		if (score >= 80 ) {
-			System.out.println("A");
-		}
-		else if (score >= 70) {
-			System.out.println("B");
-		}
-		else if (score >= 60 ) {
-			System.out.println("C");
-		}
-		else if (score >= 50 ) {
-			System.out.println("D");
-		}
-		else {
-			System.out.println("F");
-		}
-		input.close();
-	}
+        Scanner input = new Scanner(System.in);
+
+         System.out.print("Enter your score : ");
+         int score = input.nextInt();
+
+         String grade;
+         if (score >= 80) {
+             grade = "A";
+         } else if (score >= 70) {
+             grade = "B";
+         } else if (score >= 60) {
+             grade = "C";
+         } else if (score >= 50) {
+             grade = "D";
+         } else {
+             grade = "F";
+         }
+
+         System.out.printf("grade : %s", grade);
+
+         input.close();
+
+ }
+
 }
