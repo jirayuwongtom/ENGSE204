13baf59f9f1cf2497fb634f8adafb6de7920b7cd
jirayuwongtom <lovelava600@gmail.com>
2025-11-22 16:20:51 +0700

    Submit lab

--- Lab1/src/Lab1/Lab1_12.java
+++ Lab1/src/Lab1/Lab1_12.java
@@ -1,10 +1,43 @@
 package Lab1;
-
+import java.util.Scanner;
 public class Lab1_12 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		int x;
+		
+		System.out.print("Enter number : ");
+		
+		x = input.nextInt();
+		int[]id = new int[x];
+		int[]stock = new int[x];
+		System.out.println("----------------" );
+		
+		for (int i = 0; i < x; i++) {
+			System.out.print("Enter ID : ");
+			id[i] = input.nextInt();
+			System.out.print("Enter Stock : ");
+			stock[i] = input.nextInt();
+		}
+		
+		boolean check = false;
+		int search;
+		System.out.println("----------------" );
+		System.out.print("Enter Search ID : ");
+		search = input.nextInt();
+		
+		for (int i = 0; i < x; i++) {
+			if (id[i] == search) {
+				System.out.println("Stock : " + stock[i]);
+				check = true;
+			}
+		}
+		
+		if (check == false) {
+			System.out.println("Product " + search + " not found");
+		}
+		
+		input.close();
 	}
 
 }
