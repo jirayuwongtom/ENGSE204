cea15ec2db1eddcda70c05c07bbdaeb76e8c708f
jirayuwongtom <lovelava600@gmail.com>
2025-11-22 10:28:25 +0700

    Submit Lab

--- Lab1/src/Lab1/Lab1_7.java
+++ Lab1/src/Lab1/Lab1_7.java
@@ -9,12 +9,14 @@ public class Lab1_7 {
 		Scanner input = new Scanner(System.in);
 		x = input.nextInt();
 		
+		System.out.println("----------------" );
+		
 		for (int i = 0; i < x; i++) {
 			num = input.nextInt();
 			sum = sum + num;
 		}
 		
-		System.out.println(sum);
+		System.out.println("Sum :  " + sum);
 		input.close();
 	}
 
