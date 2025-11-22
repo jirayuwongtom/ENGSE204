ff65f8c40392c8d85151471e6fbaaa85370bb948
jirayuwongtom <lovelava600@gmail.com>
2025-11-22 16:42:03 +0700

    Submit lab

---
+++ Lab1/src/Lab1/Lab1_13.java
@@ -0,0 +1,36 @@
+package Lab1;
+
+import java.util.Scanner;
+
+public class Lab1_13 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int x;
+		int y;
+		
+		x = input.nextInt();
+		y = input.nextInt();
+		
+		int grid[][] = new int[x][y];
+		
+		for (int i =0; i < x; i++) {
+			for (int j = 0; j < y; j++) {
+				grid[i][j] = input.nextInt();
+			}
+		}
+		
+		int count = 0;
+		for (int i =0; i < x; i++) {
+			for (int j = 0; j < y; j++) {
+				if (grid[i][j] == 1) {
+					count++;
+				}
+			}
+		}
+		System.out.println("Online : " + count);
+		
+		input.close();
+	}
+}
+
---
+++ Lab1/src/Lab1/Lab1_14.java
@@ -0,0 +1,9 @@
+package Lab1;
+
+public class Lab1_14 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
