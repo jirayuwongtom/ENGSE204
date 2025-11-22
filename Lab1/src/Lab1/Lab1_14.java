5c42eda537f15d326489713564708948c388b17d
jirayuwongtom <lovelava600@gmail.com>
2025-11-22 17:26:32 +0700

    Submit lab

--- Lab1/src/Lab1/Lab1_14.java
+++ Lab1/src/Lab1/Lab1_14.java
@@ -1,9 +1,35 @@
 package Lab1;
-
+import java.util.Scanner;
 public class Lab1_14 {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int r;
+		int c;
+		int targetR;
+		int targetC;
 		
+		r = input.nextInt();
+		c = input.nextInt();
+		
+		char[][]map = new char[r][c];
+		
+		for (int i = 0; i < r; i++) {
+			for (int j = 0; j < c; j++) {
+				map[i][j] = input.next().charAt(0);
+			}
+		}
+		
+		System.out.print("Enter target : ");
+		targetR = input.nextInt();
+		targetC = input.nextInt();
+		
+		if (map[targetR][targetC] == '*') {
+			System.out.println("Mine");
+		}
+				
+		
+		input.close();
 	}
 
 }
