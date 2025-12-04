5e445ae0267ba6d86e13cef766044b058cf2c642
jirayuwongtom <lovelava600@gmail.com>
2025-12-04 16:40:36 +0700

    Submit lab

--- Lab3/src/Lab3_02/Player.java
+++ Lab3/src/Lab3_02/Player.java
@@ -1,9 +1,35 @@
 package Lab3_02;
+import java.util.Scanner;
 
 public class Player {
-
+	private int score;
+	
+	public Player(int initialScore) {
+		this.score = initialScore;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public void setScore(int newScore) {
+		this.score = newScore;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		int initialScore;
+		int newScore;
+		
+		initialScore = input.nextInt();
+		newScore = input.nextInt();
+		
+		Player p = new Player(initialScore);
+		
+		p.setScore(newScore);
+		System.out.println(p.getScore());
+		input.close();
 
 	}
 
