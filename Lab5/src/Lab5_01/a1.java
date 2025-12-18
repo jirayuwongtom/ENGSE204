ce53c329c97ed8bf7ccacb67a5767661cd6d10c5
jirayuwongtom <lovelava600@gmail.com>
2025-12-18 11:59:34 +0700

    Submit lab

---
+++ Lab5/src/Lab5_01/a1.java
@@ -0,0 +1,10 @@
+package Lab5_01;
+
+public class a1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Lab5/src/Lab5_02/a2.java
@@ -0,0 +1,5 @@
+package Lab5_02;
+
+public class a2 {
+
+}
